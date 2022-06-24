1b59f8675aa4ba964296f36860733b2f4972bc80
roey1234r <roey1234r@gmail.com>
2022-06-24 17:12:35 +0200

    Portfolio

--- index.php
+++ index.php
@@ -114,32 +114,31 @@
               <div class="col-lg-6">
                 <ul>
                   <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>21 Nov 1995</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.RoeyBenHarush.com</span>
-                  </li>
                   <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>0526265517</span></li>
                   <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Petah Tikwa, ISREAL</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>GitHub:</strong> <span><a href="https://github.com/roey1234r" target="_blank">Roey Ben Harush</a></span></li>
                 </ul>
               </div>
               <div class="col-lg-6">
                 <ul>
                   <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>26</span></li>
                   <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Student</span></li>
-                  <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>Roey1234r@gmail.com</span>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>Roey1234r@gmail.com</span>
                   </li>
                   <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
                 </ul>
               </div>
+              <a href="assets/CV/Roey_Ben_Harush_CV.pdf" class="downloadBtn" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-download"
+              viewBox="0 0 16 16">
+              <path
+                d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+              <path
+                d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+            </svg>
+            Open CV
+          </a>
             </div>
-            <a href="assets/CV/Roey_Ben_Harush_CV.pdf" class="downloadBtn" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-download"
-                viewBox="0 0 16 16">
-                <path
-                  d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
-                <path
-                  d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
-              </svg>
-              Open CV
-            </a>
           </div>
         </div>
 
